6de8bced71366c6b1c8cebc58d100a205549e063
Tahlil <bsse0803@iit.du.ac.bd>
2023-06-13 23:48:44 +0600

    Create WIF privatekey for litecoin mainnet

--- index.ts
+++ index.ts
@@ -1,3 +1,32 @@
 import dotenv from 'dotenv';
 
 dotenv.config();
+
+import { createHash } from 'crypto';
+import * as bs58 from 'bs58';
+
+function convertPrivateKeyToWIF(privateKey: string): string {
+  const prefix = Buffer.from([0x80]);
+  const suffix = Buffer.from([0x01]);
+
+  // Step 1: Add prefix and suffix
+  const extendedKey = Buffer.concat([prefix, Buffer.from(privateKey, 'hex'), suffix]);
+
+  // Step 2: Calculate checksum
+  const checksum = createHash('sha256').update(extendedKey).digest();
+  const doubleChecksum = createHash('sha256').update(checksum).digest();
+  const checksumBytes = doubleChecksum.slice(0, 4);
+
+  // Step 3: Append checksum
+  const extendedKeyWithChecksum = Buffer.concat([extendedKey, checksumBytes]);
+
+  // Step 4: Convert to Base58
+  const wifPrivateKey = bs58.encode(extendedKeyWithChecksum);
+
+  return wifPrivateKey;
+}
+
+// Example usage
+const privateKey: string = process.env.PRIVATE_KEY + "";
+const wifPrivateKey = convertPrivateKeyToWIF(privateKey);
+console.log('WIF Private Key:', wifPrivateKey);
